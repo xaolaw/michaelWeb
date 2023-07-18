11a9b0650a032ef16cb0dd5740931a6b858acfa3
Adam Ćwikła <adam6xaolaw@gmail.com>
2023-07-18 18:54:20 +0200

    added hamburger menu

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -9,21 +9,33 @@ import Projects from "./Projects"
 
 function Layout() {
   const [activeLink, setActiveLink] = useState('home');
+  const [showMenu, setShowMenu] = useState(false);
 
   const handleLinkClick = (link) => {
     setActiveLink(link);
+    if(showMenu==true){
+      setShowMenu(false);
+    }
   };
+  const handleHamburger = (show) =>{
+    setShowMenu(show);
+  }
   return (
     <>
       <nav className="nav-bar">
-        <ul className="list-nav-bar">
-          <img className="logo" src="src\images\smarthydro.png" alt="LOGO"></img>
+        <img className="logo" src="src\images\smarthydro.png" alt="LOGO"></img>
+        <ul className={`list-nav-bar ${showMenu == true ? 'active' : ''}`}>
           <li className={`link-li ${activeLink === 'home' ? 'show' : ''}`} onClick={()=>handleLinkClick('home')}><Link to="/">Home</Link></li>
           <li className={`link-li ${activeLink === 'about' ? 'show' : ''}`} onClick={()=>handleLinkClick('about')}><Link to="/about">About</Link></li>
           <li className={`link-li ${activeLink === 'offers' ? 'show' : ''}`} onClick={()=>handleLinkClick('offers')}><Link to="/offers">Offers</Link></li>
           <li className={`link-li ${activeLink === 'projects' ? 'show' : ''}`} onClick={()=>handleLinkClick('projects')}><Link to="/projects">Projects</Link></li>
           <li className={`link-li ${activeLink === 'contact' ? 'show' : ''}`} onClick={()=>handleLinkClick('contact')}><Link to="/contact">Contact</Link></li>
         </ul>
+        <div className={`hamburger ${showMenu == true ? 'active' : ''}`} onClick={()=>handleHamburger(!showMenu)}>
+          <span className='bar'></span>
+          <span className='bar'></span>
+          <span className='bar'></span>
+        </div>
       </nav>
       <div className="content">
 
